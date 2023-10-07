0952c7d5187f9862a61294e53137e0d2f0a20059
tlfdong <1559895216@qq.com>
2023-10-07 12:31:24 +0800

    初始化项目

---
+++ src/main/java/com/example/share/ShareApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.share;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShareApiApplication.class, args);
+    }
+
+}
