529e7029aed12e196f01b726b0c9724cf3613328
tlfdong <1559895216@qq.com>
2023-10-07 16:18:10 +0800

    演示 gateway 过滤器的使用

---
+++ share-gateway/src/main/java/top/dong/share/gateway/config/Test1Filter.java
@@ -0,0 +1,30 @@
+package top.dong.share.gateway.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author mqxu
+ * @date 2023/10/7
+ * @description Test1Filter
+ **/
+@Component
+@Slf4j
+public class Test1Filter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("Test1Filter");
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
