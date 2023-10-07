ede3a8c2696b799922bd1a0ea84f1c240b26c91d
tlfdong <1559895216@qq.com>
2023-10-07 12:40:50 +0800

    新增 share-user 模块

---
+++ share-user/src/main/java/top/dong/share/user/config/UserApplication.java
@@ -0,0 +1,41 @@
+package top.dong.share.user.config;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+@ComponentScan("top.dong")
+@Slf4j
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(UserApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        log.info("启动成功！！！");
+        log.info("测试地址 :http://127.0.0.1:{}{}/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
+
+
+
+
+
+    }
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
