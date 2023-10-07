b409572302d643057aa7ddb98e0e68b355cbec24
tlfdong <1559895216@qq.com>
2023-10-07 13:01:40 +0800

    增加 common 公共模块

--- share-common/src/main/java/top/dong/share/common/aspect/LogAspect.java
+++ share-common/src/main/java/top/dong/share/common/aspect/LogAspect.java
@@ -1,14 +1,12 @@
-package top.dong.share.user.aspect;
+package top.dong.share.common.aspect;
 
 
 import cn.hutool.core.util.RandomUtil;
 import com.alibaba.fastjson.JSONObject;
-import com.alibaba.fastjson.serializer.PropertyPreFilter;
 import com.alibaba.fastjson.support.spring.PropertyPreFilters;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -18,13 +16,10 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.MDC;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.util.Random;
 
 @Component
 @Slf4j
