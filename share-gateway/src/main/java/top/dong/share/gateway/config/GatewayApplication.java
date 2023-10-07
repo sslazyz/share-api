ef0e36b92681125cf03e4a3d885f860053acd0a0
tlfdong <1559895216@qq.com>
2023-10-07 13:41:42 +0800

    增加网关模块 gateway 并配置路由转发

---
+++ share-gateway/src/main/java/top/dong/share/gateway/config/GatewayApplication.java
@@ -0,0 +1,20 @@
+package top.dong.share.gateway.config;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+@ComponentScan("top.dong")
+@Slf4j
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(GatewayApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        log.info("网关启动成功！！");
+        log.info("网关地址:http://127.0.0.1:{}", env.getProperty("server.port"));
+    }
+}
