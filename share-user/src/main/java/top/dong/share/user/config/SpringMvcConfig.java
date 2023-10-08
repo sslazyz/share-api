d1bdc16490b65320b4c0a5eeea4e065074691ce3
tlfdong <1559895216@qq.com>
2023-10-08 14:02:14 +0800

    增加日志拦截器，统一打印日志流水号

---
+++ share-user/src/main/java/top/dong/share/user/config/SpringMvcConfig.java
@@ -0,0 +1,21 @@
+package top.dong.share.user.config;
+
+
+import jakarta.annotation.Resource;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import top.dong.share.common.interceptor.LogInterceptor;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    private LogInterceptor logInterceptor;
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor);
+    }
+}
